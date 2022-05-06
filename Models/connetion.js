cd7c4e65c9c971c2063212c6b6f310ebe6f0b24b
Carlos Silva <carlos.silva2@telefonica.com>
2022-05-06 13:15:39 -0300

    Adding env model and fix connection

--- Models/connetion.js
+++ Models/connetion.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const Sequelize =  require('sequelize')
 const Op = Sequelize.Op;
 const operatorsAliases = {
@@ -6,14 +5,12 @@ const operatorsAliases = {
   $not: Op.not
 }
 
-
-const dbLocal = process.env.DB_LOCAL
-const dbLanguage  = process.env.DB_LANGUEGE
+const dbLocal = process.env.DB_LOCAL || './DB/register_green_teens.sqlite3'
+const dbLanguage  = process.env.DB_LANGUAGE || 'sqlite'
 
 const sequelize = new Sequelize({
-    // host: 'localhost',
-    dialect: 'sqlite',
-    storage: './DB/register_green_teens.sqlite3',
+    dialect: dbLanguage,
+    storage: dbLocal,
     operatorsAliases
 })
 
